3d71cd5f0a66d4f19a4c41f2582162fab6ecb6c9
serdaraydin <serdara2000@gmail.com>
2024-05-21 00:29:15 +0300

    supabase prisma OK

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -2,6 +2,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -10,6 +11,8 @@ import { Label } from "@/components/ui/label";
 import { SelectCategory } from "../components/SelectCategory";
 import { Textarea } from "@/components/ui/textarea";
 import { TipTapEditor } from "../components/Editor";
+import { UploadDropzone } from "../lib/uploadthing";
+import { Button } from "@/components/ui/button";
 
 export default function SellRoute() {
   return (
@@ -43,7 +46,18 @@ export default function SellRoute() {
               <Label>Description</Label>
               <TipTapEditor />
             </div>
+            <div className="flex flex-col gap-y-2">
+              <Label>Product Images</Label>
+              <UploadDropzone endpoint="imageUploader" />
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <Label>Product Files</Label>
+              <UploadDropzone endpoint="productFileUpload" />
+            </div>
           </CardContent>
+          <CardFooter className="mt-5">
+            <Button>Submit Form</Button>
+          </CardFooter>
         </form>
       </Card>
     </section>
